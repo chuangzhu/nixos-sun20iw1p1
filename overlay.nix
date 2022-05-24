ffc04b84c585cb45ff8c5ce78e9818df5e70b45c
Chuang Zhu <git@chuang.cz>
2022-05-24 12:59:06 +0800

    use fetchpatch

--- overlay.nix
+++ overlay.nix
@@ -2,11 +2,22 @@ final: prev:
 
 rec {
   opensbi = prev.opensbi.overrideAttrs (super: {
-    patches = [ ./0001-lib-utils-fdt-Require-match-data-to-be-const.patch ./0002-lib-utils-timer-Add-a-separate-compatible-for-the-D1.patch ];
+    patches = [
+      (prev.fetchpatch {
+        name = "lib-utils-fdt-Require-match-data-to-be-const.patch";
+        url = "https://github.com/smaeul/opensbi/commit/d53ad4541869f058e806133d7d5653d014b89b76.patch";
+        sha256 = "sha256-EKaSbpH/Oc5gVJN2EpVnUnAt0AUinm0C9hgiaMV3ro4=";
+      })
+      (prev.fetchpatch {
+        name = "lib-utils-timer-Add-a-separate-compatible-for-the-D1.patch";
+        url = "https://github.com/smaeul/opensbi/commit/4bcaf9aa1dbc69ccb68998c7d462ef895163d493.patch";
+        sha256 = "sha256-sqQtWBRREx+xs30S/kzNQG6Us0ZjYlGrmSb/LHUBaPI=";
+      })
+    ];
   });
-  sun20i-d1-spl = prev.pkgs.callPackage ./spl.nix { };
-  ubootLicheeRV = prev.pkgs.callPackage ./uboot.nix { };
-  linux_nezha = prev.pkgs.callPackage ./linux.nix { };
+  sun20i-d1-spl = prev.callPackage ./spl.nix { };
+  ubootLicheeRV = prev.callPackage ./uboot.nix { };
+  linux_nezha = prev.callPackage ./linux.nix { };
   linuxPackages_nezha = packagesFor linux_nezha;
 
   packagesFor = kernel:
